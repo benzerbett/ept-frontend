6522237734d86733dd6675b1c8d588dc256dfeeb
Benzer <benzerbett@gmail.com>
2022-09-09 22:13:34 +0300

    Cleanup

--- components/common/AppNavbar.js
+++ components/common/AppNavbar.js
@@ -6,67 +6,6 @@ function AppNavbar() {
 
     const router = useRouter()
     return (
-        // <nav style={{background: '#acd', padding: '1rem'}}>
-        //     <ul style={{listStyle: 'none', display: 'flex', justifyContent: 'flex-start', gap: '9px'}}>
-        //         <li>
-        //             Auth: 
-        //         </li>
-        //         <li>
-        //             <Link href="/auth/login">Log in</Link>
-        //         </li>
-        //         <li>
-        //             <Link href="/auth/signup">Sign up</Link>
-        //         </li>
-        //         <li>
-        //             <Link href="/auth/password-reset">Password reset</Link>
-        //         </li>
-        //     </ul>
-        //     <ul style={{listStyle: 'none', display: 'flex', justifyContent: 'flex-start', gap: '9px'}}>
-        //         <li>
-        //             Admin: 
-        //         </li>
-        //         <li>
-        //             <Link href="/">Home</Link>
-        //         </li>
-        //         <li>
-        //             <Link href="/admin/users">Users</Link>
-        //         </li>
-        //         <li>
-        //             <Link href="/admin/access/permissions">Permissions</Link>
-        //         </li>
-        //         <li>
-        //             <Link href="/admin/access/roles">Roles</Link>
-        //         </li>
-        //         <li>
-        //             <Link href="/admin/dictionary">Dictionary</Link>
-        //         </li>
-        //         <li>
-        //             <Link href="/admin/forms">Forms</Link>
-        //         </li>
-        //         <li>
-        //             <Link href="/admin/schemes">Schemes</Link>
-        //         </li>
-        //     </ul>
-        //     <ul style={{listStyle: 'none', display: 'flex', justifyContent: 'flex-start', gap: '9px'}}>
-        //         <li>
-        //             User: 
-        //         </li>
-        //         <li>
-        //             <Link href="/user/evaluations">Evaluations</Link>
-        //         </li>
-        //         <li>
-        //             <Link href="/user/reports">Reports</Link>
-        //         </li>
-        //         <li>
-        //             <Link href="/user/surveys">Surveys</Link>
-        //         </li>
-        //         <li>
-        //             <Link href="/user/settings">Settings</Link>
-        //         </li>
-        //     </ul>
-        // </nav>
-
-
 
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
@@ -132,7 +71,7 @@ function AppNavbar() {
                         </li>
                         <li className="nav-item mx-md-2 dropdown d-flex">
                             <div className="btn-group">
-                                <a className={"nav-link " + (router.pathname.includes('access/') ? ' active' : '')} aria-current="page">Access control</a>
+                                <a className={"nav-link " + ( router.pathname.includes('/permissions') || router.pathname.includes('/roles') ? ' active' : '' )} aria-current="page">Access control</a>
                                 <button type="button" className="btn btn-xs btn-transparent text-white dropdown-toggle dropdown-toggle-split p-0 my-0 btn-outline-dark" data-bs-toggle="dropdown" aria-expanded="false">
                                     <span className="visually-hidden">Toggle Dropdown</span>
                                 </button>
