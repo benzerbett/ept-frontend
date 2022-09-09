6d103020d36a7295aa6c1ddde2f515d1b9f5a1a8
Benzer <benzerbett@gmail.com>
2022-09-09 20:32:58 +0300

    Added Bootstrap 5

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.css'
+
 import PublicTheme from '../components/layouts/PublicTheme'
 import AppTheme from '../components/layouts/AppTheme'
 import { useState } from 'react'
+import Head from 'next/head'
+import Script from 'next/script'
+
 
 function MyApp({ Component, pageProps }) {
     const [isLoggedIn, setIsLoggedIn] = useState(true)
     const Layout = isLoggedIn ? AppTheme : PublicTheme
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+            <Script src="/js/bootstrap.bundle.min.js"/>
+
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
     )
 }
 
