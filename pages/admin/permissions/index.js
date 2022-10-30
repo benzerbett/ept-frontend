8513e9fb328dae67ce9759f31fa8e9f2032bddfb
Benzer <benzerbett@gmail.com>
2022-10-30 12:23:48 +0300

    Paging: trial in permissions listing

--- pages/admin/permissions/index.js
+++ pages/admin/permissions/index.js
@@ -7,6 +7,17 @@ import { doGetSession, getResource } from '../../../utilities'
 function Permissions() {
     const [permissions, setPermissions] = useState([])
     const [session, setSession] = useState([])
+
+    const [page, setPage] = useState(1)
+    const [lastPage, setLastPage] = useState(1)
+    const [totalPages, setTotalPages] = useState(1)
+    const [totalRecords, setTotalRecords] = useState(0)
+    const [count, setCount] = useState(0)
+    const [perPage, setPerPage] = useState(10)
+    const [pageLinks, setPageLinks] = useState([])
+
+    const [search, setSearch] = useState('')
+
     const [status, setStatus] = useState('')
     const [message, setMessage] = useState('')
     const [loading, setLoading] = useState(true);
@@ -20,9 +31,20 @@ function Permissions() {
                 if (session) {
                     setSession(session)
                     // fetch permissions
-                    getResource('permissions').then((data) => {
+                    let rsc = 'permissions?page='+page
+                    getResource(rsc).then((data) => {
                         if (data.status === true) {
-                            setPermissions(data?.data)
+                            setPermissions(data?.data?.data)
+
+                            // set page
+                            setPage(data?.data?.current_page)
+                            setTotalPages(data?.data?.last_page)
+                            setPerPage(data?.data?.per_page)
+                            setTotalRecords(data?.data?.total)
+                            setLastPage(data?.data?.last_page)
+                            setPageLinks(data?.data?.links)
+                            setCount(data?.data?.data.length || 0)
+
                             setStatus('')   // ('success')
                             setMessage('')  // ('Permissions fetched successfully')
                         } else {
@@ -42,7 +64,7 @@ function Permissions() {
             })
         }
         return () => mounted = false
-    }, [])
+    }, [page])
 
 
     if (loading) return <main style={{ width: '100%', height: '85vh', overflow: 'hidden', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
@@ -65,6 +87,7 @@ function Permissions() {
                 <title>EPT | Permissions</title>
                 <meta name="description" content="EPT" />
                 <link rel="icon" href="/favicon.ico" />
+                <meta charSet="utf-8" />
             </Head>
             <div className="container">
                 <div className="d-flex flex-column flex-md-row justify-content-between align-items-center">
@@ -76,63 +99,102 @@ function Permissions() {
                         </form>
                     </div>
                 </div>
-                <div className="d-flex w-100">
-                    <div className="table-responsive w-100">
-                        <table className="table table-striped table-hover table-bordered">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Description</th>
-                                    <th scope="col">Status</th>
-                                    <th scope="col">Created At</th>
-                                    <th scope="col">Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {permissions && permissions.length > 0 ? permissions.map((permission) => (
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="d-flex w-100">
+                            <div className="table-responsive w-100">
+                                <table className="table table-striped table-hover table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Name</th>
+                                            <th scope="col">Description</th>
+                                            <th scope="col">Status</th>
+                                            <th scope="col">Created At</th>
+                                            <th scope="col">Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {permissions && permissions.length > 0 ? permissions.map((permission) => (
 
-                                    <tr key={permission.uuid}>
-                                        <td>
-                                            <Link href={`/admin/permissions/${permission.uuid}`}>
-                                                <a className="">{permission.name}</a>
-                                            </Link>
-                                        </td>
-                                        <td>
-                                            {permission?.description}
-                                        </td>
-                                        <td className='text-capitalize'>{permission.deleted_at == null ? <span className='badge bg-success'>Active</span> : <span className='badge bg-warning'>Disabled</span>}</td>
-                                        <td>{new Date(permission?.created_at).toLocaleString('en-GB', {
-                                            year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true
-                                        }) || "-"}</td>
-                                        <td className="d-flex flex-column flex-md-row gap-2 justify-content-center">
-                                            <Link href={{ pathname: `/permission/${permission.uuid}/edit` }} >
-                                                <a className='btn btn-primary btn-sm py-0 text-nowrap'>Edit Permission</a>
-                                            </Link>
-                                            <a className='btn text-danger btn-link btn-sm py-0 text-nowrap' onClick={ev => {
-                                                ev.preventDefault();
-                                                ev.stopPropagation();
-                                                if (confirm('Are you sure you want to delete this permission?')) {
-                                                    getResource(`permissions/delete/${permission.uuid}`, { uuid: permission.uuid }).then((data) => {
-                                                        if (data.status === true) {
-                                                            setStatus('success')
-                                                            setMessage('Permission deleted successfully')
-                                                            setPermissions(permissions.filter(u => u.uuid !== permission.uuid))
-                                                        } else {
-                                                            setStatus('error')
-                                                            setMessage(data.message)
+                                            <tr key={permission.uuid}>
+                                                <td>
+                                                    <Link href={`/admin/permissions/${permission.uuid}`}>
+                                                        <a className="">{permission.name}</a>
+                                                    </Link>
+                                                </td>
+                                                <td>
+                                                    {permission?.description}
+                                                </td>
+                                                <td className='text-capitalize'>{permission.deleted_at == null ? <span className='badge bg-success'>Active</span> : <span className='badge bg-warning'>Disabled</span>}</td>
+                                                <td>{new Date(permission?.created_at).toLocaleString('en-GB', {
+                                                    year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true
+                                                }) || "-"}</td>
+                                                <td className="d-flex flex-column flex-md-row gap-2 justify-content-center">
+                                                    <Link href={{ pathname: `/permission/${permission.uuid}/edit` }} >
+                                                        <a className='btn btn-primary btn-sm py-0 text-nowrap'>Edit</a>
+                                                    </Link>
+                                                    <a className='btn text-danger btn-link btn-sm py-0 text-nowrap' onClick={ev => {
+                                                        ev.preventDefault();
+                                                        ev.stopPropagation();
+                                                        if (confirm('Are you sure you want to delete this permission?')) {
+                                                            getResource(`permissions/delete/${permission.uuid}`, { uuid: permission.uuid }).then((data) => {
+                                                                if (data.status === true) {
+                                                                    setStatus('success')
+                                                                    setMessage('Permission deleted successfully')
+                                                                    setPermissions(permissions.filter(u => u.uuid !== permission.uuid))
+                                                                } else {
+                                                                    setStatus('error')
+                                                                    setMessage(data.message)
+                                                                }
+                                                            }).catch((err) => {
+                                                                console.log(err)
+                                                                setStatus('error')
+                                                                setMessage('Error deleting permission: ' + err.message || err)
+                                                            })
                                                         }
-                                                    }).catch((err) => {
-                                                        console.log(err)
-                                                        setStatus('error')
-                                                        setMessage('Error deleting permission: ' + err.message || err)
-                                                    })
+                                                    }}>Delete</a>
+                                                </td>
+                                            </tr>
+                                        )) : <tr><td colSpan={7} className="text-center">No permissions found</td></tr>}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-md-12'>
+                        {/* pagination */}
+                        <div className="d-flex justify-content-between align-items-center">
+                            <nav aria-label='Pagination'>
+                                <ul className="pagination">
+                                    {/* <li className="page-item">
+                                        <button disabled={true} className="page-link" onClick={ev => {
+                                            ev.preventDefault(); ev.stopPropagation(); setPage(page - 1)
+                                        }}>Previous</button>
+                                    </li> */}
+                                    {pageLinks.map((link, index) => (
+                                        <li key={index} className={`page-item ${link.active ? 'active' : ''}`}>
+                                            {true && <button className="page-link" onClick={ev => {
+                                                ev.preventDefault(); ev.stopPropagation();
+                                                if (link.url) {
+                                                    setPage(link.url.split('page=')[1])
                                                 }
-                                            }}>Delete Permission</a>
-                                        </td>
-                                    </tr>
-                                )) : <tr><td colSpan={7} className="text-center">No permissions found</td></tr>}
-                            </tbody>
-                        </table>
+                                            }}>
+                                                {link.label.split(' ').filter(w=>!w.includes('&')).join(' ')}
+                                            </button>}
+                                        </li>
+                                    ))}
+                                    {/* {<li className="page-item">
+                                        <button className="page-link" disabled={
+                                            page == lastPage
+                                        } onClick={ev => { ev.preventDefault(); ev.stopPropagation(); setPage(page + 1) }}>Next</button>
+                                    </li>} */}
+                                </ul>
+                            </nav>
+                            <div className='d-flex align-items-end flex-column justify-content-center'>
+                                <small>Showing {page} of {totalPages} pages</small>
+                                <small className="text-muted">{count} of {totalRecords} items</small>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
